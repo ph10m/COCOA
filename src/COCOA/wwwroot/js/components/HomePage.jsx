86d2065f4329d797c3999f3f9bdcd9e4803ecff9
Tobias Kullblikk <TobiasKullblikk@gmail.com>
2017-02-07 14:46:13 +0100

    Initial project setup.

---
+++ src/COCOA/wwwroot/js/components/HomePage.jsx
@@ -0,0 +1,20 @@
+﻿var Button = ReactBootstrap.Button;
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello COCOA!</p>
+                <Button bsStyle="success">Lets rumble!</Button>
+            </div>
+        );
+    }
+
+    handleRumbleClick(e) {
+        console.log("Rumbling!");
+    }
+}
