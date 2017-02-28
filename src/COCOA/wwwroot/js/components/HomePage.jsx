8d43d213820b013eb3216c34c7911eccc4a1aec8
Morteano <morteano@stud.ntnu.no>
2017-02-28 15:14:14 +0100

    add simple panels with mockup data to home view

--- src/COCOA/wwwroot/js/components/HomePage.jsx
+++ src/COCOA/wwwroot/js/components/HomePage.jsx
@@ -1,16 +1,33 @@
 ﻿var PageHeader = ReactBootstrap.PageHeader;
+var Panel = ReactBootstrap.Panel;
 
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
+        
     }
-
+    clickedPanel() {
+        alert('You have clicked on me');
+    }
+    
     render() {
+        var data = [];
+        data.push(JSON.parse('{ "header": "TDT4145", "text": "tralala" }'));
+        data.push(JSON.parse('{ "header": "TDT4145", "text": "blablabla" }'));
+        var elementList = data.map((element) => {
+            return (
+                <Panel header={element.header}>
+                    {element.text}
+                </Panel>
+            );
+        })
+
         return (
 
             <div>
                 <CocoaHeader/>
                 <PageHeader>Welcome to COCOA!</PageHeader>
+                {elementList}
             </div>
             
         );
