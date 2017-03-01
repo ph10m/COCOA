a0b9ccad3bbc0f7a14518d02f41727157f2940d6
Morteano <morteano@stud.ntnu.no>
2017-03-01 21:04:02 +0100

    add a scrollable div around the panels, add testing of adding and removing panels

--- src/COCOA/wwwroot/js/components/HomePage.jsx
+++ src/COCOA/wwwroot/js/components/HomePage.jsx
@@ -4,17 +4,30 @@ var Panel = ReactBootstrap.Panel;
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
-        
+        this.data = [JSON.parse('{ "header": "TDT4145", "text": "tralala" }')];
+        this.indexTest = 4100;
     }
     clickedPanel() {
         alert('You have clicked on me');
     }
+
+    addPanel() {
+        //this.data.push(JSON.parse('{ "header": "TDT4145", "text": "tralala" }'));
+        //this.data.push(JSON.parse('{ "header": "TDT4145", "text": "blablabla" }'));
+        this.data.unshift((JSON.parse('{ "header": "TDT'+this.indexTest+'", "text": "lalala" }')));
+        this.forceUpdate();
+        this.indexTest++;
+        console.log(this.data);
+    }
+
+   removePanel() {
+        this.data.pop();
+        this.forceUpdate();
+        console.log(this.data);
+    }
     
     render() {
-        var data = [];
-        data.push(JSON.parse('{ "header": "TDT4145", "text": "tralala" }'));
-        data.push(JSON.parse('{ "header": "TDT4145", "text": "blablabla" }'));
-        var elementList = data.map((element) => {
+        var elementList = this.data.map((element) => {
             return (
                 <Panel header={element.header}>
                     {element.text}
@@ -27,7 +40,10 @@ class HomePage extends React.Component {
             <div>
                 <CocoaHeader/>
                 <PageHeader>Welcome to COCOA!</PageHeader>
-                {elementList}
+                <button onClick={this.addPanel.bind(this)}>Add panel</button>
+                <button onClick={this.removePanel.bind(this)}>Remove panel</button>
+                <div className="scroll">{elementList}</div>
+                <button onClick={this.addPanel.bind(this)}>Add panel</button>
             </div>
             
         );
