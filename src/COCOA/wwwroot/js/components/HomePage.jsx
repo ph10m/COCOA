1304db4006cd2ed460fe94d00fac871b24cdc5af
Morteano <morteano@stud.ntnu.no>
2017-03-03 13:14:44 +0100

    remove panel from list of data and update the view, instead of removing the html component

--- src/COCOA/wwwroot/js/components/HomePage.jsx
+++ src/COCOA/wwwroot/js/components/HomePage.jsx
@@ -4,18 +4,19 @@ var Panel = ReactBootstrap.Panel;
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
-        this.data = [JSON.parse('{ "header": "TDT4145", "text": "tralala" }')];
-        this.indexTest = 4100;
         this.id = 0;
+        this.data = [JSON.parse('{ "id": "0", "header": "TDT4145", "text": "tralala" }')];
+        this.indexTest = 4100;
     }
     clickedPanel() {
         alert('You have clicked on me');
     }
 
     addPanel() {
+        this.id++;
         //this.data.push(JSON.parse('{ "header": "TDT4145", "text": "tralala" }'));
         //this.data.push(JSON.parse('{ "header": "TDT4145", "text": "blablabla" }'));
-        this.data.unshift((JSON.parse('{ "header": "TDT'+this.indexTest+'", "text": "lalala" }')));
+        this.data.unshift((JSON.parse('{ "id": '+this.id+', "header": "TDT'+this.indexTest+'", "text": "lalala" }')));
         this.forceUpdate();
         this.indexTest++;
         console.log(this.data);
@@ -27,15 +28,26 @@ class HomePage extends React.Component {
         console.log(this.data);
    }
 
-   onClickPanel(clickedPanel) {
-       var panel = clickedPanel.target.parentNode
-       panel.parentNode.removeChild(panel);
+   onClickPanel(id) {
+       this.data.splice(this.getPanelIndexFromId(id.target.parentNode.id), 1);
+       this.forceUpdate();
+       //var panel = clickedPanel.target.parentNode
+       //panel.parentNode.removeChild(panel);
+   }
+
+   getPanelIndexFromId(id) {
+       console.log(id);
+       for (var i = 0; i < this.data.length; i++) {
+           if (this.data[i].id == id) {
+               return i;
+           }
+       }
    }
     
     render() {
         var elementList = this.data.map((element) => {
             return (
-                <Panel header={element.header} onClick={this.onClickPanel.bind(this)}>
+                <Panel id={element.id} header={element.header} onClick={this.onClickPanel.bind(this) }>
                     {element.text}
                 </Panel>
             );
