38d33c53f028486e7d7682cb4b7c7f9e92f1e294
Morteano <morteano@stud.ntnu.no>
2017-03-06 20:17:47 +0100

    only display scrollbar when needed and remove prints

--- src/COCOA/wwwroot/js/components/HomePage.jsx
+++ src/COCOA/wwwroot/js/components/HomePage.jsx
@@ -8,9 +8,6 @@ class HomePage extends React.Component {
         this.data = [JSON.parse('{ "id": "0", "header": "TDT4145", "text": "tralala" }')];
         this.indexTest = 4100;
     }
-    clickedPanel() {
-        alert('You have clicked on me');
-    }
 
     addPanel() {
         this.id++;
@@ -19,24 +16,21 @@ class HomePage extends React.Component {
         this.data.unshift((JSON.parse('{ "id": '+this.id+', "header": "TDT'+this.indexTest+'", "text": "lalala" }')));
         this.forceUpdate();
         this.indexTest++;
-        console.log(this.data);
     }
 
    removePanel() {
         this.data.pop();
         this.forceUpdate();
-        console.log(this.data);
    }
 
-   onClickPanel(id) {
-       this.data.splice(this.getPanelIndexFromId(id.target.parentNode.id), 1);
+   onClickPanel(element) {
+       this.data.splice(this.getPanelIndexFromId(element.target.parentNode.id), 1);
        this.forceUpdate();
        //var panel = clickedPanel.target.parentNode
        //panel.parentNode.removeChild(panel);
    }
 
    getPanelIndexFromId(id) {
-       console.log(id);
        for (var i = 0; i < this.data.length; i++) {
            if (this.data[i].id == id) {
                return i;
@@ -47,7 +41,7 @@ class HomePage extends React.Component {
     render() {
         var elementList = this.data.map((element) => {
             return (
-                <Panel id={element.id} header={element.header} onClick={this.onClickPanel.bind(this) }>
+                <Panel id={element.id} header={element.header} bsStyle="primary" onClick={this.onClickPanel.bind(this) }>
                     {element.text}
                 </Panel>
             );
